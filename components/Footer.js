86e10ba4fe681be23f7c83685652e6bc8e53df2b
Muhammad Anas <anassohail34343@gmail.com>
2023-11-30 13:14:40 -0800

    globalcss,navbar,footer
    Global css for the entire app has been written and foote and navbar page has been created

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../src/styles/Footer.module.css';
+
+export const Footer = () => {
+  return (
+    <div className={styles.container}>Footer</div>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </>
+  );
 }
