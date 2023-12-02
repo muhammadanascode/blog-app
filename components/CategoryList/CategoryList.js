5b3ccfff45720ba8f3a913198cb83e430ec14db3
Muhammad Anas <anassohail34343@gmail.com>
2023-12-02 15:21:19 -0800

    Featured and category
    Featured is made and responsive and category component is designed completely,

--- components/CategoryList/CategoryList.js
+++ components/CategoryList/CategoryList.js
@@ -1,10 +1,94 @@
-import React from 'react'
-import styles from './CategoryList.module.css'
+import React from "react";
+import styles from "./CategoryList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
   return (
-    <div>CategoryList</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href={"/blog?cat=style"}
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src={"/style.jpg"}
+            width={32}
+            height={32}
+            alt="styleimg"
+            className={styles.image}
+          />
+          Style
+        </Link>
+        <Link
+          href={"/blog?cat=fashion"}
+          className={`${styles.category} ${styles.fashion}`}
+        >
+          <Image
+            src={"/fashion.jpeg"}
+            width={32}
+            height={32}
+            alt="fashionimg"
+            className={styles.image}
+          />
+          Fashion
+        </Link>
+        <Link
+          href={"/blog?cat=food"}
+          className={`${styles.category} ${styles.food}`}
+        >
+          <Image
+            src={"/food.jpeg"}
+            width={32}
+            height={32}
+            alt="foodimg"
+            className={styles.image}
+          />
+          Food
+        </Link>
+        <Link
+          href={"/blog?cat=travel"}
+          className={`${styles.category} ${styles.travel}`}
+        >
+          <Image
+            src={"/travel.jpeg"}
+            width={32}
+            height={32}
+            alt="travelimg"
+            className={styles.image}
+          />
+          Travel
+        </Link>
+        <Link
+          href={"/blog?cat=culture"}
+          className={`${styles.category} ${styles.culture}`}
+        >
+          <Image
+            src={"/culture.png"}
+            width={32}
+            height={32}
+            alt="cultureimg"
+            className={styles.image}
+          />
+          Culture
+        </Link>
+        <Link
+          href={"/blog?cat=coding"}
+          className={`${styles.category} ${styles.coding}`}
+        >
+          <Image
+            src={"/coding.jpeg"}
+            width={32}
+            height={32}
+            alt="codingeimg"
+            className={styles.image}
+          />
+          Coding
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default CategoryList
+export default CategoryList;
--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -6,25 +6,32 @@ import AuthLink from "../AuthLinks/AuthLink";
 
 export const Navbar = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.social}>
-        <Image src={"/fblogo.png"} alt="fb" width={24} height={24} />
-        <Image src={"/instalogo.jpeg"} alt="insta" width={24} height={24} />
-        <Image src={"/linkedlogo.png"} alt="linkedin" width={24} height={24} />
+    <>
+      <div className={styles.container}>
+        <div className={styles.social}>
+          <Image src={"/fblogo.png"} alt="fb" width={24} height={24} />
+          <Image src={"/instalogo.jpeg"} alt="insta" width={24} height={24} />
+          <Image
+            src={"/linkedlogo.png"}
+            alt="linkedin"
+            width={24}
+            height={24}
+          />
+        </div>
+        <div className={styles.logo}>ScriptSynergy</div>
+        <div className={styles.links}>
+          <Link className={styles.link} href={"/"}>
+            Home
+          </Link>
+          <Link className={styles.link} href={"/contact"}>
+            Contact
+          </Link>
+          <Link className={styles.link} href={"/about"}>
+            About
+          </Link>
+          <AuthLink />
+        </div>
       </div>
-      <div className={styles.logo}>ScriptSynergy</div>
-      <div className={styles.links}>
-        <Link className={styles.link} href={"/"}>
-          Home
-        </Link>
-        <Link className={styles.link} href={"/contact"}>
-          Contact
-        </Link>
-        <Link className={styles.link} href={"/about"}>
-          About
-        </Link>
-        <AuthLink />
-      </div>
-    </div>
+    </>
   );
 };
