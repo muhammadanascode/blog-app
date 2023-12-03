2a4fb90555585d105e5d1da49f98c0f2dcc5dd98
Muhammad Anas <anassohail34343@gmail.com>
2023-12-02 17:22:45 -0800

    CardList
    CardList component component has been designed without responsiveness

--- components/CardList/CardList.js
+++ components/CardList/CardList.js
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./CardList.module.css";
 import Pagination from "../Pagination/Pagination";
+import Card from "../Card/Card";
 
 const CardList = () => {
   return (
-    <div>
-      CardList
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
       <Pagination />
     </div>
   );
