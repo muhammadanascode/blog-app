f9880f3cdf5276265c0d29f0a8292e31c7b07bb1
Muhammad Anas <anassohail34343@gmail.com>
2023-12-13 12:37:46 +0500

    connection adn scripts updated

--- utils/connect.js
+++ utils/connect.js
@@ -2,13 +2,18 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+try {
+  if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+  } else {
+    if (!global.prisma) {
+      global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
   }
-  prisma = global.prisma;
+} catch (error) {
+  // Handle Prisma Client initialization error
+  console.error("Error initializing Prisma Client:", error);
 }
 
 export default prisma;
