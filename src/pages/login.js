25aaa6fb899f85985390f884eac475485f22d0e3
Muhammad Anas <anassohail34343@gmail.com>
2023-12-14 13:00:13 +0500

    update login.js
    useeffectt removed

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,28 +5,30 @@ import styles from "../styles/login.module.css";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { IoPersonSharp } from "react-icons/io5";
-import {toast } from "react-toastify";
+import { toast } from "react-toastify";
 
 const Login = () => {
   const { status } = useSession();
   const router = useRouter();
   console.log(status);
 
-  useEffect(() => {
-    if (status === "authenticated") {
-      router.replace("/");
-      toast.success("Logged in successfully", {
-        position: "top-center",
-        autoClose: 500,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "light",
-      });
-    }
-  }, [status, router]);
+  // useEffect(() => {
+
+  // }, [status, router]);
+
+  if (status === "authenticated") {
+    router.replace("/");
+    toast.success("Logged in successfully", {
+      position: "top-center",
+      autoClose: 500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  }
 
   if (status === "loading") {
     return "Loading plase wait...";
@@ -35,7 +37,10 @@ const Login = () => {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-     <div className={styles.person}> <IoPersonSharp className={styles.img} /> </div>
+        <div className={styles.person}>
+          {" "}
+          <IoPersonSharp className={styles.img} />{" "}
+        </div>
         <div className={styles.socialButton} onClick={() => signIn("google")}>
           Signin with google
         </div>
