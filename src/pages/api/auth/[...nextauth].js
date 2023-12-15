88f2c67b2e966d450f575a07327cba795a4c2107
Muhammad Anas <anassohail34343@gmail.com>
2023-12-15 15:28:14 +0500

    update [...nextauth].js
    call back url inculded in api

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,6 +3,11 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "../../../../utils/connect";
 
+const callbackUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://scriptsynergy.vercel.app/api/auth/callback/google"
+    : "http://localhost:3000/api/auth/callback/google";
+
 export const authOptions = {
   // Configure one or more authentication providers
   secret: process.env.NEXTAUTH_SECRET,
@@ -11,6 +16,8 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      scope: "openid profile email",
+      callbackUrl,
     }),
   ],
 };
